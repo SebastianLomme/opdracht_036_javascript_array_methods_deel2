1ee602e20aab685d7cb307939bff2e981b808d11
Sebastian Lomme <sebastian.lomme@gmail.com>
2021-08-11 14:57:42 +0200

    eerste init

---
+++ script.js
@@ -0,0 +1,10 @@
+const superheroes = [
+    {name: "Batman", alter_ego: "Bruce Wayne"}, 
+    {name: "Superman", alter_ego: "Clark Kent"}, 
+    {name: "Spiderman", alter_ego: "Peter Parker"}]
+    
+    // Hier komt jouw functie
+    
+    console.log(findSpiderMan(superheroes)) 
+    // Find Spiderman
+    // result should be: {name: "Spiderman", alter_ego: "Peter Parker"}
